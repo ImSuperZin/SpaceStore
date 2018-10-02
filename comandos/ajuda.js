f94fe8a0170d78e3efd2e953572583b958b877bf
ImSuperZin <39243594+ImSuperZin@users.noreply.github.com>
2018-10-02 19:09:31 -0300

    Update ajuda.js

--- comandos/ajuda.js
+++ comandos/ajuda.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
-    message.reply(`Enviei minhas informações em seu privado , olhe lá`).then(msg => msg.delete(6000));
+    message.reply(`Enviei minhas informações em seu privado , olhe lá :wave:`).then(msg => msg.delete(6000));
     const msg1 = new Discord.RichEmbed()
     .setAuthor(message.author.username, message.author.displayAvatarURL)
     .setColor('RANDOM')
@@ -28,22 +28,22 @@ module.exports.run = async (client, message, args) => {
                 const embed = new Discord.RichEmbed()
                 .setAuthor("🔩 Comandos de Moderação")
                 .setColor('RANDOM')
-                .setDescription(`**ban** \`<@user>\` \`<motivo>\` - Para banir um membro do servidor.
-                \n**kick** \`<@user>\` \`<motivo>\` - Para kickar um membro do servidor.
+                .setDescription(`**h!ban** \`<@user>\` \`<motivo>\` - Para banir um membro do servidor.
+                \n**-ick** \`<@user>\` \`<motivo>\` - Para kickar um membro do servidor.
                 \n**aviso** \`<mensagem do anúncio>\` - Para avisar algo na loja.
                 \n**limpar** \`<quantia>\` - Para você limpar o chat.
                 \n**votacao** \`<msg>\` - Para você abrir uma votação.
                 \n**say** \`<msg>\` - Irá mandar mensagens com o bot.
                 \n**parceiro** \`<msg>\` - Você ira mandar alguma nova parceria da loja.
                 \n**mute** \`<@user>\` \`<motivo>\` - Irá mutar o usuário mencionado.
-                \n**unmute** \`<@user>\` - Irá desmutar o usuário mencionado.`
+                \n**unmute** \`<@user>\` - Irá desmutar o usuário mencionado.
                 message.member.send({embed: embed}).then(a=>a.delete(15000));
             })
             grl.on('collect', r=>{
                 const glr = new Discord.RichEmbed()
                 .setAuthor("📋 Outros comandos")
                 .setColor('RANDOM')
-                .setDescription(`**ajuda** - Mostre meus comandos!.
+                .setDescription(`**h!ajuda** - Onde mostra todos os comandos do bot.
                 \n**avatar** - Para você baixar seu avatar.
                 \n**avatar** \`<@user>\` - Para você baixar o avatar do usuário mencionado.
                 \n**serverinfo** - Para ver as informações sobre a loja.`)
